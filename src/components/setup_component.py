be4906e180d5b181c98a5c8d349fe9d05513f0d9
Garcia Souza <garcia.souza@easycodeit.com.br>
2024-11-17 20:05:00 -0300

    wip

--- src/components/setup_component.py
+++
@@ -1,9 +0,0 @@
-from src.services.setup_service import from_bfm_get_setup, from_bfm_post_setup
-
-def process_get_setup():
-    response = from_bfm_get_setup()
-    return response
-
-def process_post_setup(setup):
-    response = from_bfm_post_setup(setup)
-    return response
--- src/services/setup_service.py
+++
@@ -1,6 +0,0 @@
-
-def from_bfm_get_setup():
-    return {"id": 123, "settings": "abc"}
-
-def from_bfm_post_setup(setup):
-    return {}
